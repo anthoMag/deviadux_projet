d0f0a5254c66848926d29eb873177967b268d03c
anthoMag <119311321+anthoMag@users.noreply.github.com>
2022-12-19 16:55:32 +0100

    update

--- public/asset/js/home.js
+++ public/asset/js/home.js
@@ -1,5 +1,5 @@
 /* ---------Anim SVG----------- */
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', (e)=>{
 
     let prevTime = 0;
     let prevTime2 = 0;
@@ -8,12 +8,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
         let image = document.getElementById('img-anim-hexa');
         let rect = image.getBoundingClientRect();
 
-        if (rect.y < window.innerHeight - rect.height && rect.y > 1) {
+        if(rect.y < window.innerHeight-rect.height && rect.y > 1) {
             let delay = image.dataset['delay'];
             let now = Date.now();
             let ecoule = now - prevTime;
 
-            if (ecoule > (delay * 1000)) {
+            if (ecoule > (delay*1000)) {
                 image.src = "./logopictoicone/hexaanimate.svg";
                 prevTime = now;
             }
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
         image = document.getElementById('img-anim-pluie');
         rect = image.getBoundingClientRect();
 
-        if (rect.y < window.innerHeight - rect.height && rect.y > 1) {
+        if(rect.y < window.innerHeight-rect.height && rect.y > 1) {
             let delay = image.dataset['delay'];
             let now = Date.now();
             let ecoule = now - prevTime2;
 
-            if (ecoule > (delay * 1000)) {
+            if (ecoule > (delay*1000)) {
                 image.src = "./logopictoicone/pluieanimate.svg";
                 prevTime2 = now;
             }
@@ -39,12 +39,35 @@ document.addEventListener('DOMContentLoaded', (e) => {
 let closeBtn = document.getElementById('closeCookie');
 
 
-closeBtn.addEventListener('click', close);
 
-function close() {
-
-    let cookieBar = document.getElementById('barCookies');
-
-    cookieBar.style.display = 'none';
-
+function close(){
+    
+    let cookieBar = document.getElementById('cookiesBar');
+    
+    cookieBar.style.display='none';
+    
 }
+
+closeBtn.addEventListener('click',close);
+/* ---------------------------modal------------------------------------- */
+
+// var modal = document.querySelector(".modal");
+// var trigger = document.querySelector(".example");
+// var closeButton = document.querySelector(".close-button");
+
+// function toggleModal() {
+//   modal.classList.toggle("show-modal");
+//   el = document.getElementById("example");
+//   el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
+// }
+
+// function windowOnClick(event) {
+//   if (event.target === modal) {
+//     toggleModal();
+//   }
+// }
+
+
+// trigger.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
