d4440bf965d9a6b0106cfaf75638e6363110b9a1
Joël MAYEMBA <joelmayemba@gmail.com>
2023-01-10 15:11:44 +0100

    Update Full_deviadux_database.sql

--- public/asset/sql/Full_deviadux_database.sql
+++ public/asset/sql/Full_deviadux_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 09 jan. 2023 à 12:50
+-- Généré le : mar. 10 jan. 2023 à 14:11
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -42,15 +42,15 @@ CREATE TABLE IF NOT EXISTS `departements` (
 --
 
 INSERT INTO `departements` (`id`, `name`, `code_dep`, `reg_id`) VALUES
-(97, 'Ain', '01', 1),
-(98, 'Aisne', '02', 7),
-(99, 'Allier', '03', 1),
-(100, 'Alpes-de-Haute-Provence', '04', 13),
-(101, 'Hautes-Alpes', '05', 13),
-(102, 'Alpes-Maritimes', '06', 13),
+(97, 'Ain', '1', 1),
+(98, 'Aisne', '2', 7),
+(99, 'Allier', '3', 1),
+(100, 'Alpes-de-Haute-Provence', '4', 13),
+(101, 'Hautes-Alpes', '5', 13),
+(102, 'Alpes-Maritimes', '6', 13),
 (103, 'Ardèche', '07', 1),
 (104, 'Ardennes', '08', 6),
-(105, 'Ariège', '09', 11),
+(105, 'Ariège', '9', 11),
 (106, 'Aube', '10', 6),
 (107, 'Aude', '11', 11),
 (108, 'Aveyron', '12', 11),
@@ -159,7 +159,8 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 ('DoctrineMigrations\\Version20230106002131', '2023-01-06 00:21:35', 146),
-('DoctrineMigrations\\Version20230106003133', '2023-01-06 00:31:40', 104);
+('DoctrineMigrations\\Version20230106003133', '2023-01-06 00:31:40', 104),
+('DoctrineMigrations\\Version20230110135351', '2023-01-10 13:53:59', 205);
 
 -- --------------------------------------------------------
 
@@ -180,6 +181,8 @@ CREATE TABLE IF NOT EXISTS `evenements` (
   `coordonnee_y` double DEFAULT NULL,
   `dep_id` int(11) DEFAULT NULL,
   `polygones` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image_2` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` text COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_E10AD400814AA86C` (`dep_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -188,18 +191,18 @@ CREATE TABLE IF NOT EXISTS `evenements` (
 -- Déchargement des données de la table `evenements`
 --
 
-INSERT INTO `evenements` (`id`, `title`, `image`, `periode`, `level_alert`, `pic_de_crue`, `lieu`, `coordonnee_x`, `coordonnee_y`, `dep_id`, `polygones`) VALUES
-(1, 'Crues de l’Adour moyen et de la Midouze', 'mont_de_marsant.jpg', '1er au 5 janvier 2021', 'orange', 7.45, 'Mont-de-Marsan', 43.8915, -0.498008, 137, NULL),
-(2, 'Crues de l’Arrats-Gimone-Save-Touch', 'beaumont_de_lomagne.jpg', '1er au 5 janvier 2021', 'orange', 3.05, ' Beaumont-de-Lomagne', 43.8811, 0.992782, 179, NULL),
-(5, 'Crues de la Charentonne', 'trinite.jpg', '22 au 24 juin 2021', 'orange', 2.43, 'Trinité-de-Réville', 48.9701, 0.5082, 124, NULL),
-(6, 'Crues de l’Aisne Vaux', 'rilly_sur_aisne.jpg', '14 au 24 juillet 2021', 'orange', 5.4, 'Rilly-sur-Aisne', 49.49, 4.63309, 104, NULL),
-(8, 'Crues de l’Ardèche ', 'gravieres.jpg', '2 au 4 octobre 2021', 'orange', 6.32, 'Les Salelles', 44.4252, 4.09502, 103, NULL),
-(10, 'Crues de la Lys', 'merville.jpg', '28 novembre au 2 décembre 2021', 'orange', 2.88, 'Merville', 50.6421, 2.64489, 156, NULL),
-(11, 'Crues du Bec du Gave', 'cambo_les_bains.jpg', '9 au 15 décembre', 'rouge', 5.78, 'Cambo-les-Bains', 43.3631, -1.3976, 161, NULL),
-(12, 'Crue de la saôme', 'macon.jpg', '20 au 22 juillet', 'orange', 4.81, 'Macon', 46.302723, 4.834103, 168, NULL),
-(14, 'Crue de la Loire', 'angers_2019.jpg', '4 au 5 février 2021', 'orange', 4.79, 'Angers', 47.476965, -0.561218, 146, NULL),
-(16, 'Crue du Gardon', 'anduze.jpg', '2 au 5 ocobre 2021', 'orange', 3.85, 'Anduze', 44.055711, 3.987693, 127, NULL),
-(17, 'Crue de la Garonne', 'reole.jpg', '2 au 5 ocobre 2021', 'orange', 9.5, 'La Réole', 44.579336, -0.034024, 130, NULL);
+INSERT INTO `evenements` (`id`, `title`, `image`, `periode`, `level_alert`, `pic_de_crue`, `lieu`, `coordonnee_x`, `coordonnee_y`, `dep_id`, `polygones`, `image_2`, `link`) VALUES
+(1, 'Crues de l’Adour moyen et de la Midouze', 'mont_de_marsant.jpg', '1er au 5 janvier 2021', 'orange', 7.45, 'Mont-de-Marsan', 43.8915, -0.498008, 137, '[]', '', ''),
+(2, 'Crues de l’Arrats-Gimone-Save-Touch', 'beaumont_de_lomagne.jpg', '1er au 5 janvier 2021', 'orange', 3.05, ' Beaumont-de-Lomagne', 43.8811, 0.992782, 179, '[]', '', ''),
+(5, 'Crues de la Charentonne', 'trinite.jpg', '22 au 24 juin 2021', 'orange', 2.43, 'Trinité-de-Réville', 48.9701, 0.5082, 124, '[]', '', ''),
+(6, 'Crues de l’Aisne Vaux', 'rilly_sur_aisne.jpg', '14 au 24 juillet 2021', 'orange', 5.4, 'Rilly-sur-Aisne', 49.49, 4.63309, 104, '[]', '', ''),
+(8, 'Crues de l’Ardèche ', 'gravieres.jpg', '2 au 4 octobre 2021', 'orange', 6.32, 'Les Salelles', 44.4252, 4.09502, 103, '[]', '', ''),
+(10, 'Crues de la Lys', 'merville.jpg', '28 novembre au 2 décembre 2021', 'orange', 2.88, 'Merville', 50.6421, 2.64489, 156, '[]', '', ''),
+(11, 'Crues du Bec du Gave', 'cambo_les_bains.jpg', '9 au 15 décembre', 'rouge', 5.78, 'Cambo-les-Bains', 43.3631, -1.3976, 161, '[]', '', ''),
+(12, 'Crue de la saôme', 'macon.jpg', '20 au 22 juillet', 'orange', 4.81, 'Macon', 46.302723, 4.834103, 168, '[]', '', ''),
+(14, 'Crue de la Loire', 'angers_2019.jpg', '4 au 5 février 2021', 'orange', 4.79, 'Angers', 47.476965, -0.561218, 146, '[]', '', '\"https://files.geo.data.gouv.fr/link-proxy/catalogue.sigloire.fr/2020-11-13/5faddb6a6a9fcb45c5f4c0c4/file.zip/telechargement-1604748779-7263/2981eead-479f-4b73-9139-68618c3776dd-1604748779-9408/pdf-complet-n-tri-ange2013-inondable-s-049.zip/territoires-a-risques-important-dinondation-tri-de-angers-val-dauthion-saumur-surfaces-inondables/template-de-catalogue-dattributs.pdf\"'),
+(16, 'Crue du Gardon', 'anduze.jpg', '2 au 5 ocobre 2021', 'orange', 3.85, 'Anduze', 44.055711, 3.987693, 127, '[]', '', ''),
+(17, 'Crue de la Garonne', 'reole.jpg', '2 au 5 ocobre 2021', 'orange', 9.5, 'La Réole', 44.579336, -0.034024, 130, '[]', '', '');
 
 -- --------------------------------------------------------
 
